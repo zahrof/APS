ccf2210f206112de62b7f04ed9e2c7b75bae6d5e
Hernouf Mohamed <hernouf@yandex.ru>
2020-02-27 18:32:40 +0100

    Type checking is finished. To integrate Java API for Prolog

--- APS0/prolog/judjement.pl
+++ APS0/prolog/judjement.pl
@@ -1,51 +1,103 @@
-/* auxiliaire*/
+/* Relations auxilliaire */
+
+/* Concatenation de deux listes */
 append([],X,X).
 append([A|X],Y,[A|Result]):-
         append(X,Y,Result).
-
+/* Verification si le cle K exitste dans contexte. 
+Si existe on retourne le type associe, sinon undefined*/
+lookup([],_,undefined).
+lookup([(K,V)|_], K, V).
+lookup([_|T], K, Result):-
+        lookup(T,K,Result).
+/* Renvoie la liste de types des arguments. Par exemple:
+   [x:int,y:int,z:bool] -> [int,int,bool]*/
 typeArgs([],[]).
 typeArgs([(_,Type)|Rest],[Type | Result]):- 
         typeArgs(Rest,Result).
-
+/* Renvoie la liste de types des expressions. Par exemple:
+   [num(1),true,num(5)] -> [int,bool,int]*/
 typeListExpressions(_, [],[]).
 typeListExpressions(Context,[Expr|Rest],[Type|Result]):-
         typeExpression(Context,Expr,Type),
         typeListExpressions(Context,Rest,Result).
+/* Context initial */
+context_init([ (add,([int,int],int)), 
+               (sub,([int,int],int)), 
+               (mul,([int,int],int)), 
+               (div,([int,int],int)),
+               (and,([bool,bool],bool)),
+               (or,([bool,bool],bool)),
+               (not,([bool],bool)),
+               (eq,([int,int],bool)),
+               (lt,([int,int],bool)),
+               (true,bool),
+               (false,bool) ]).
 
-/*regle NUM*/
-typeExpressionNum(Context,num(X),int).
+/* Typage d'expressions */
 
-/*regle SYM*/
-typeExpressionSym([],K, undefined).
-typeExpressionSym([(var(K),V)|T],var(K),V).
-typeExpressionSym([(K,V)|T], K,V):-!.
-typeExpressionSym([_|T],K,Result):-
-        typeExpressionSym(T,K,Result).
-
+/* regle NUM */
+typeExpression(_,num(_),int):- !.
+/* regle SYM (pour var) */
+typeExpression(Context, var(Var), Result):-
+        lookup(Context,Var,Result), !.
 /*regle ABS*/
-
-typeExpressionAbs(Context, abs(Args,Corps), (TypeArgs,Type)):-
-        append(Context,Args,New_Context),
+typeExpression(Context, abs(Args,Corps), (TypeArgs,Type)):-
+        append(Args,Context,New_Context),
         typeArgs(Args,TypeArgs),
-        typeExpression(New_Context, Corps, Type).
-
+        typeExpression(New_Context, Corps, Type), !.
 /* régle APP */
-typeExpressionApp(Context, app(Invoc, Exprs), Type):-
+typeExpression(Context, app(Invoc,Exprs), Type):-
         typeListExpressions(Context,Exprs,Types),
-        typeExpression(Context, Invoc, (Types,Type)).
-
+        typeExpression(Context, Invoc, (Types,Type)), !.
 /* regle IF */ 
-typeExpressionIf(Context, if(Cond, Expr1, Expr2), Result):-
+typeExpression(Context, if(Cond,Expr1,Expr2), Result):-
         typeExpression(Context, Cond, bool),
         typeExpression(Context, Expr1, Result),
-        typeExpression(Context, Expr2, Result).  
+        typeExpression(Context, Expr2, Result), !.  
+/* regle SYM (pour oprim) ou undefined (pour expression mal typé) */
+typeExpression(Context, X, Result):-
+        lookup(Context,X,Result), !.
 
+/* Typage de statements */
 
-/* regles Expr */
+/* regle ECHO */
+typeStatement(Context, echo(Expr), void):-
+        typeExpression(Context, Expr, int), !.
 
-typeExpression(Context,num(X),Result):- typeExpressionNum(Context, num(X),Result), !.
-typeExpression(Context,var(X),Result):- typeExpressionSym(Context, var(X),Result), !.
-typeExpression(Context, abs(Args,Corps), Result):- typeExpressionAbs(Context, abs(Args,Corps), Result), !.
-typeExpression(Context, app(Invoc,Exprs), Result):- typeExpressionApp(Context, app(Invoc,Exprs), Result), !.
-typeExpression(Context, if(Cond, Expr1, Expr2), Result) :- typeExpressionIf(Context,if(Cond, Expr1, Expr2), Result), !.
-typeExpression(Context,X,Result):- typeExpressionSym(Context, X,Result).
+/* Typage de declarations */
+
+/* regle CONST */
+typeDeclaration(Context, const(Var,Type,Expr), New_Context):-
+        typeExpression(Context, Expr, Type),
+        append([(Var,Type)],Context, New_Context), !.
+/* regle FUNC */
+typeDeclaration(Context, func(Var,Type,Args,Expr), New_Context):-
+        append(Args,Context,FuncContext),
+        typeExpression(FuncContext,Expr,Type),
+        typeArgs(Args,TypeArgs),
+        append([(Var,(TypeArgs,Type))], Context, New_Context), !.
+/* regle FUNCREC */        
+typeDeclaration(Context, func_rec(Var,Type,Args,Expr), New_Context):-
+        typeArgs(Args,TypeArgs),
+        append([(Var,(TypeArgs, Type)) | Args],Context,FuncRecContext),
+        typeExpression(FuncRecContext,Expr,Type),
+        append([(Var,(TypeArgs,Type))], Context, New_Context), !.
+
+/* Typage de commandes */
+
+/* regle END */
+typeCommands(_, [], void).
+/* regle STATS */
+typeCommands(Context, [Stat|Cmds], void):-
+        typeStatement(Context, Stat, void),
+        typeCommands(Context, Cmds, void).
+/* regle DECS */
+typeCommands(Context, [Dec|Cmds], void):-
+        typeDeclaration(Context, Dec, New_Context),
+        typeCommands(New_Context, Cmds, void).
+
+/* Typage du programme */
+typeProgram(prog(Cmds)):-
+        context_init(Context0),
+        typeCommands(Context0, Cmds, void), !.
--- APS0/src/PrologTerm.java
+++ APS0/src/PrologTerm.java
@@ -23,7 +23,7 @@ public class PrologTerm implements IAstVisitor<String, Void, Exception> {
 
 	@Override
 	public String visit(AstArgument ast, Void data) throws Exception {
-		return ast.getName() + ":" + ast.getType().accept(this, data);
+		return "("+ast.getName() + "," + ast.getType().accept(this, data)+")";
 	}
 
 	@Override
@@ -38,63 +38,66 @@ public class PrologTerm implements IAstVisitor<String, Void, Exception> {
 
 	@Override
 	public String visit(AstConst ast, Void data) throws Exception {
-		return "CONST " + ast.getName() + " " + ast.getType().accept(this, data) + " "
-				+ ast.getBody().accept(this, data);
+		return "const( " + ast.getName() + ", " + ast.getType().accept(this, data) + ", "
+				+ ast.getBody().accept(this, data)+" )";
 	}
 
 	@Override
 	public String visit(AstEcho ast, Void data) throws Exception {
-		return "ECHO " + ast.getExpr().accept(this, data);
+		return "echo( " + ast.getExpr().accept(this, data)+" )";
 	}
 
 	@Override
 	public String visit(AstFun ast, Void data) throws Exception {
 		StringBuilder sb = new StringBuilder();
-		sb.append("FUN " + ast.getName() + " ");
+		sb.append("func( " + ast.getName() + ", "+ast.getType().accept(this, data)+", ");
 		sb.append("[");
 		for (AstArgument arg : ast.getArguments()) {
 			sb.append(arg.accept(this, data));
 			sb.append(", ");
 		}
-		sb.delete(sb.length() - 2, sb.length() - 1);
-		sb.append("] ");
+		sb.delete(sb.length() - 2, sb.length());
+		sb.append("], ");
 		sb.append(ast.getBody().accept(this, data));
+		sb.append(" )");
 		return sb.toString();
 	}
 
 	@Override
 	public String visit(AstFuncType ast, Void data) throws Exception {
 		StringBuilder sb = new StringBuilder();
-		sb.append("(");
+		sb.append("([");
 		for (AstType arg : ast.getTypesArguments()) {
 			sb.append(arg.accept(this, data));
-			sb.append(" * ");
+			sb.append(",");
 		}
-		sb.delete(sb.length() - 3, sb.length() - 1);
-		sb.append(") -> ");
+		sb.delete(sb.length() - 1, sb.length());
+		sb.append("],");
 		sb.append(ast.getTypeResult().accept(this, data));
+		sb.append(")");
 		return sb.toString();
 	}
 
 	@Override
 	public String visit(AstFunRec ast, Void data) throws Exception {
 		StringBuilder sb = new StringBuilder();
-		sb.append("FUN rec" + ast.getName() + " ");
+		sb.append("func_rec( " + ast.getName() + ", "+ast.getType().accept(this, data)+", ");
 		sb.append("[");
 		for (AstArgument arg : ast.getArguments()) {
 			sb.append(arg.accept(this, data));
 			sb.append(", ");
 		}
-		sb.delete(sb.length() - 2, sb.length() - 1);
-		sb.append("] ");
+		sb.delete(sb.length() - 2, sb.length());
+		sb.append("], ");
 		sb.append(ast.getBody().accept(this, data));
+		sb.append(")");
 		return sb.toString();
 	}
 
 	@Override
 	public String visit(AstIf ast, Void data) throws Exception {
-		return "(if " + ast.getCondition().accept(this, data) + " " + ast.getConsequence().accept(this, data) + " "
-				+ ast.getAlternative().accept(this, data) + ")";
+		return "if( " + ast.getCondition().accept(this, data) + ", " + ast.getConsequence().accept(this, data) + ", "
+				+ ast.getAlternative().accept(this, data) + " )";
 	}
 
 	@Override
@@ -105,63 +108,64 @@ public class PrologTerm implements IAstVisitor<String, Void, Exception> {
 	@Override
 	public String visit(AstLambda ast, Void data) throws Exception {
 		StringBuilder sb = new StringBuilder();
-		sb.append("[");
+		sb.append("abs( [");
 		for (AstArgument arg : ast.getArguments()) {
 			sb.append(arg.accept(this, data));
 			sb.append(", ");
 		}
-		sb.delete(sb.length() - 2, sb.length() - 1);
-		sb.append("] ");
+		sb.delete(sb.length() - 2, sb.length());
+		sb.append("], ");
 		sb.append(ast.getBody().accept(this, data));
+		sb.append(" )");
 		return sb.toString();
 	}
 
 	@Override
 	public String visit(AstNum ast, Void data) throws Exception {
-		return ast.getInteger().toString();
+		return "num("+ast.getInteger().toString()+")";
 	}
 
 	@Override
 	public String visit(AstOp ast, Void data) throws Exception {
 		StringBuilder sb = new StringBuilder();
-		sb.append("("+ast.getOperation()+" ");
+		sb.append("app( "+ast.getOperation()+", [");
 		for (AstExpression expr : ast.getExpressions()) {
 			sb.append(expr.accept(this, data));
-			sb.append(" ");
+			sb.append(", ");
 		}
-		sb.deleteCharAt(sb.length()-1);
-		sb.append(")");
+		sb.delete(sb.length() - 2, sb.length());
+		sb.append("] )");
 		return sb.toString();
 	}
 
 	@Override
 	public String visit(AstProgramme ast, Void data) throws Exception {
 		StringBuilder sb = new StringBuilder();
-		sb.append("[");
+		sb.append("prog( [");
 		for (AstCommand cmd : ast.getCommands()) {
 			sb.append(cmd.accept(this, data));
-			sb.append("; ");
+			sb.append(", ");
 		}
-		sb.delete(sb.length() - 2, sb.length() - 1);
-		sb.append("]");
+		sb.delete(sb.length() - 2, sb.length());
+		sb.append("] )");
 		return sb.toString();
 	}
 
 	@Override
 	public String visit(AstVar ast, Void data) throws Exception {
-		return ast.getName();
+		return "var("+ast.getName()+")";
 	}
 
 	@Override
 	public String visit(AstApplication ast, Void data) throws Exception {
 		StringBuilder sb = new StringBuilder();
-		sb.append("("+ast.getInvocable().accept(this, data)+" ");
+		sb.append("app( "+ast.getInvocable().accept(this, data)+", [");
 		for (AstExpression expr : ast.getExpressions()) {
 			sb.append(expr.accept(this, data));
-			sb.append(" ");
+			sb.append(", ");
 		}
-		sb.deleteCharAt(sb.length()-1);
-		sb.append(")");
+		sb.delete(sb.length() - 2, sb.length());
+		sb.append("] )");
 		return sb.toString();
 	}
 
